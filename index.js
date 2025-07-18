118a1eddfbd66dcc260c7dbe40be4afffe9d26f9
KevinPaez28 <Kevinandreypaezgarces@gmail.com>
2025-07-18 12:04:01 -0500

    encriptacion contrasena, creacion de tokens

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { valid } from './validador.js';
+import { valid,validLogin, validToken } from './validador.js';
 import bcrypt from "bcryptjs";
+import mysql2 from "mysql2/promise";
+import  jwt  from 'jsonwebtoken';
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,25 +14,71 @@ app.get('/', (req, res) => {
 
 })
 
+
+const conexion= async()=>{return await mysql2.createConnection({
+  host:"localhost",
+  user:"brayan_adso2894667",
+  password:"Aprendiz2025",
+  database:"brayan"
+})}
+
+
 app.post('/registro',valid,async(req,res)=>{
     const info=req.body;
-
-    console.log(info);
-    
-
+  
     const nombre=info.nombre;
     const correo=info.correo;
     const contrasena=info.contrasena;
-    console.log(contrasena);
-    
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(contrasena, salt);
-    console.log(hash);
-    
+    // console.log(await bcrypt.compare(contrasena, hash));
 
-    // console.log("hola hola");
+    const sql="INSERT INTO usuarios(nombre,email,password) VALUES (?,?,?)";
+    const atributos=[nombre,correo,hash];
+
+    const pepito=await conexion();
     
+    const consulta= await pepito.query(sql,atributos);
+
+    console.log(consulta);
+    
+})
+
+app.post('/login',validLogin,async (req,res)=>{
+  const {correo,contrasena}=req.body;
+
+  const pepito=await conexion();
+  const sql="SELECT * FROM usuarios WHERE email=?";
+  const atributos=[correo];
+
+  const consulta= await pepito.query(sql,atributos);
+
+  const user=consulta[0][0];
+  if( !await bcrypt.compare(contrasena, user.password)){
+    return res.json("No puede ingresar contraseña o correo incorrecto")
+  }
+
+  const token= jwt.sign({id:user.id,nombre:user.nombre},"palabraLlavePrivada",{expiresIn:"1m"});
+  const tokenRefresch= jwt.sign({id:user.id,nombre:user.nombre},"palabraRefresco",{expiresIn:"7d"});
+  console.log(token);
+  return res.json({token,tokenRefresch});
+  
+})
+
+app.get('/privada',validToken, (req,res)=>{
+  
+  
+})
+app.post('/refrescar',(req,res)=>{
+  const{refreshtoken} = req.body;
+  const decoded = jwt.verify(refreshtoken, 'palabraRefresco', (err,decoded)=>{
+    console.log(decoded);
+    
+  });
+  // console.log(decoded);
+  
+
 })
 
 app.listen(3000)
--- validador.js
+++ validador.js
@@ -1,3 +1,4 @@
+import  jwt  from 'jsonwebtoken';
 
 export const valid=(req,res,next)=>{
     const {nombre,correo,contrasena}=req.body;
@@ -7,4 +8,32 @@ export const valid=(req,res,next)=>{
     if(!contrasena)return res.json({error:"La contraseña no puede estar vacia"});
 
     next()
+}
+
+export const validLogin=(req,res,next)=>{
+    const {correo,contrasena}=req.body;
+
+    if(!correo)return res.json({error:"El correo no puede estar vacio"});
+    if(!contrasena)return res.json({error:"La contraseña no puede estar vacia"});
+
+    next()
+}
+
+export const validToken = (req,res,next) =>{
+    try {
+        console.log(req.headers);
+        const cabecero = req.headers;
+        if(!cabecero.authorization || !cabecero.authorization.includes("Bear")){
+            return res.json("formato no valido");
+        }
+        const arraglo = cabecero.authorization.split("   ")
+        console.log(arraglo[1]);
+        const token = arraglo[1];
+        const decoded = jwt.verify(token, 'palabraLlavePrivada');
+        console.log(decoded);
+    } catch (error) {
+        
+    }
+    
+    next();
 }
